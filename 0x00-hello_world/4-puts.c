7ae417cf1d419b778306d50e7857c4446b789723
aoko-code <cynthiakhamenge@yahoo.com>
2022-04-07 18:48:50 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 # include <stdio.h>
 /**
-* main - Entry point
-*
-* return:Always 0 (success)
-*/
+ * main - Entry point
+ *
+ * return: 0 after printing the function
+ */
 int main(void)
-	{
-		puts("\"Programming is like building a multilingual puzzle");
-		return (0);
-	}
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
 
