7a99aa39fe5d17683427d743d3fc11bbaa365580
aoko-code <cynthiakhamenge@yahoo.com>
2022-04-07 18:29:03 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,9 +4,9 @@
 *
 * return:Always 0 (success)
 */
-int main (void)
-    {
-        puts ("\"Programming is like building a multilingual puzzle");
-        return (0);
-    }
+int main(void)
+	{
+		puts("\"Programming is like building a multilingual puzzle");
+		return (0);
+	}
 
