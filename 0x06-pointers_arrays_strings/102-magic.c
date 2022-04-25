3774ec1694b2960c99e464a4475fc92443007d85
aoko-code <cynthiakhamenge@yahoo.com>
2022-04-25 03:15:02 -0700

    magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
+
 int main(void)
 {
-
 int n;
-
 int a[5];
-
 int *p;
-
 
 
 a[2] = 1024;
@@ -22,7 +19,6 @@ p = &n;
    * - only one statement
    * - you are not allowed to code anything else than this line of code
    */
-
 p[5] = 98;
 
   /* ...so that this prints 98\n */
