144c2a7b2631d8a7bb3899120ada210a91c69dc0
aoko-code <cynthiakhamenge@yahoo.com>
2022-04-29 13:53:53 -0700

    print line

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,6 +1,5 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
-
+#ifndef _MAIN_H_INCLUDED
+#define _MAIN_H_INCLUDED
 int _putchar(char);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
@@ -11,5 +10,4 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
-
 #endif
