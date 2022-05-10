bc6d95006a58af9f9a7025a6ab0a3ef710607782
aoko-code <cynthiakhamenge@yahoo.com>
2022-05-10 12:35:10 -0700

    allocate memory

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdlib.h>
 /**
  *malloc_checked - array for prints a string
  *@b: number of memory
@@ -7,8 +6,11 @@
  */
 
 void *malloc_checked(unsigned int b)
+
 {
+
 void *p;
+
 
 p = malloc(b);
 
